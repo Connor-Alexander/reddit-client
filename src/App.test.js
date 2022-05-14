183a1e78e67789b92c9fa17aac7bb8f88142f070
Camilo <cavs1010@gmail.com>
2022-05-15 05:25:47 +1000

    Logo updated

--- src/App.test.js
+++ src/App.test.js
@@ -7,14 +7,21 @@ Glossary of Enzyme: https://enzymejs.github.io/enzyme/docs/api/shallow.html
 
 import App from "./App";
 import Header from "./components/Header";
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { Nav } from "react-bootstrap";
 
 configure({ adapter: new Adapter() });
 
-describe('<Header />', () => { // What are we describing?
-  test('displays correct heading', () => { // What is the test?
-    const wrapper = shallow(<Header />);
-    expect(wrapper.find('h1').text()).toBe('Reddit-Client') // Expected output?
-  })
-})
+describe("<Header />", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Header />);
+  });
+  test("Logo is displayed", () => {
+    expect(wrapper.containsMatchingElement(<img />)).toEqual(true);
+  });
+  test("Link to home is displayed", () => {
+    expect(wrapper.containsMatchingElement(<Nav.Link />)).toEqual(true);
+  });
+});
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,32 @@
 import React from "react";
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import Logo from "../images/logo.png";
+//import {ReactComponent as }
 
 function Header() {
   return (
     <Navbar bg="dark">
-		<h1 style={{color: 'white'}}>Reddit-Client</h1>
+      <Container>
+        <Navbar.Brand>
+          <img
+            src={Logo}
+            style={{ maxWidth: "12rem", maxHeight: "4rem" }}
+            alt={"Logo"}
+          />{" "}
+          <h1
+            style={{
+              color: "white",
+              display: "inline",
+              height: "100%",
+              position:'relative',
+              top: "15px",
+            }}
+          >
+            Reddit-Client
+          </h1>
+        </Navbar.Brand>
+        <Nav.Link>Home</Nav.Link>
+      </Container>
     </Navbar>
   );
 }
